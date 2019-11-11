28b625df31501cccf8122b9502f5f273b096599f
Diego Martínez Alaminos <55781947+diegomartinezalaminos@users.noreply.github.com>
2019-11-11 13:12:18 +0100

    Add files via upload

---
+++ TypeScript/Ejercicio03/array.js
@@ -0,0 +1,45 @@
+var array = new Array(20);
+var primos = new Array();
+var noprimos = new Array();
+var contador = 0;
+var auxcontador = 0;
+var cadena = "";
+console.log("");
+console.log("EL Array aleatorio es: ");
+console.log("");
+for (var y = 0; y < 20; y++) {
+    var random = Math.floor(Math.random() * 101 + 1);
+    array[y] = random;
+    cadena = cadena + String(random) + " , ";
+}
+console.log(cadena);
+cadena = "";
+for (var i = 0; i < 20; i++) {
+    for (var x = 0; x <= array[i]; x++) {
+        if ((array[i] % x) == 0) {
+            contador++;
+        }
+    }
+    if (contador == 2) {
+        primos[auxcontador] = array[i];
+        auxcontador++;
+    }
+    else {
+        noprimos[auxcontador] = array[i];
+        auxcontador++;
+    }
+    contador = 0;
+}
+console.log("");
+console.log("El Array aleatorio con números primos al principio y no primos al final: ");
+console.log("");
+var cont = 0;
+primos.forEach(function (element) {
+    cadena = cadena + String(element) + " , ";
+    cont++;
+});
+noprimos.forEach(function (element) {
+    cadena = cadena + String(element) + " , ";
+    cont++;
+});
+console.log(cadena);
---
+++ TypeScript/Ejercicio03/array.ts
@@ -0,0 +1,54 @@
+var array:number[] = new Array(20);
+var primos:number[] = new Array();
+var noprimos:number[] = new Array();
+var contador:number = 0;
+var auxcontador:number = 0;
+var cadena:String = "";
+
+console.log("");
+console.log("EL Array aleatorio es: ");
+console.log("");
+
+for(var y = 0;y < 20;y++) {
+  var random = Math.floor(Math.random() * 101 + 1);
+  array[y] = random;
+  cadena = cadena + String(random) + " , "; 
+}
+
+console.log(cadena);
+cadena = "";
+
+for(var i = 0;i < 20;i++){
+  for(var x = 0;x <= array[i];x++){
+    if ((array[i] % x) == 0){
+      contador++;
+    }
+  }
+  if (contador == 2){
+    primos[auxcontador] = array[i];
+    auxcontador++;
+  }else{
+    noprimos[auxcontador] = array[i];
+    auxcontador++;
+  }
+  contador = 0;   
+}
+
+console.log("");
+console.log("El Array aleatorio con números primos al principio y no primos al final: ")
+console.log("");
+
+var cont:number = 0;
+primos.forEach(element => {
+  cadena = cadena + String(element) + " , ";
+  cont++;
+});
+
+noprimos.forEach(element => {
+  cadena = cadena + String(element) + " , ";
+  cont++;
+});
+
+console.log(cadena);
+
+
